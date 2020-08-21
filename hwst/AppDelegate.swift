45e84a86b8caf4ea71339a95745e4f26b5909270
Anton Prokhorov <pasinworld@gmail.com>
2020-08-21 23:57:12 +0300

    Added .gitignore

--- hwst/AppDelegate.swift
+++ hwst/AppDelegate.swift
@@ -12,11 +12,12 @@ import CoreData
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
+    private let window: UIWindow = UIWindow(frame: UIScreen.main.bounds)
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        window?.rootViewController = ViewController()
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
         
         return true
     }
