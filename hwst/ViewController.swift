97628f9c31fb4e109fdb9ea41d28786c856802b8
Anton Prokhorov <a1989p@mail.ru>
2020-08-21 22:56:46 +0300

    Initial Commit

---
+++ hwst/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hwst
+//
+//  Created by Антон Прохоров on 21.08.2020.
+//  Copyright © 2020 Anton Prokhorov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
